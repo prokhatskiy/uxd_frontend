0cbdbee04a99552489934acf159caf62b8b96a51
Artem Prokhatskiy <prokhatskiy@gmail.com>
2014-02-15 13:47:43 +0200

    stylus build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,77 +1,50 @@
 'use strict';
 
-var LIVERELOAD_PORT = 1337,
-    APP_PORT = 666,
-    lrSnippet = require('connect-livereload')({port:LIVERELOAD_PORT}),
-    mountFolder = function(connect, dir){
-        return connect.static(require("path").resolve(dir));
-    }
-
-module.exports = function(grunt) {
+module.exports = function(grunt) {      
     grunt.initConfig({
-        stylus: {
-            compile: {
-                files: {
-                    "css/styles.css" : ["styl/styles.styl" ]
-                },
-                options: {
-                    compress: false
-                }
-            }
-        },
-        connect: {
-            options: {
-                port: APP_PORT
-            },
-            livereload: {
-                options: {
-                    middleware: function (connect) {
-                        return [
-                            lrSnippet,
-                            mountFolder(connect, '')
-                        ];
-                    }
-                }
-            }
-        },
-        open: {
-            server: {
-                path: "http://localhost:" + APP_PORT
-            }
-        },
+        config : grunt.file.readJSON('config/main.json'),
         watch: {
-            options: {
-                livereload: LIVERELOAD_PORT
-            },
-            stylus: {
-                files: ["styl/*.styl" ],
-                tasks: ['stylus'],
-                options: {
-                    spawn: false
-                }
-            }
-        },
-        autoprefixer: {
-            options: {
-                browsers: ['last 1 version', 'ie 10']
+            block : {
+                files: '<%= config.blockSrc %>/*',
+                tasks: ['stylus:blocks']
             },
             css: {
-                expand: true,
-                flatten: true,
-                src: 'css/*.css',
-                dest: 'css/'
+                files: '<%= config.cssSrc %>/*',
+                tasks: ['stylus:common']
             }
         },
-        clean: {
-            css: ["css/_*.css"],
+        stylus: {
+            block: {
+                options: {
+                    linenos : '<%= config.linenos %>',
+                    urlfunc: 'embedurl',
+                    import: [     
+                        '../css/variables/variables',    
+                        '../css/mixins/mixins'
+                    ],
+                    define : grunt.file.readJSON('config/stylusVars.json')
+                },
+                files : {
+                    '<%= config.cssDist %>/blocks.css': '<%= config.blockSrc %>/blocks.styl'
+                }
+            },
+            css: {
+                options: {
+                    linenos : '<%= config.linenos %>',
+                    import: [     
+                        'variables/variables',    
+                        'mixins/mixins'
+                    ],
+                    define : grunt.file.readJSON('config/stylusVars.json')
+                },
+                files : {
+                    '<%= config.cssDist %>/main.css' : '<%= config.cssSrc %>/main.styl'
+                }
+            }
         }
     });
-
-    grunt.loadNpmTasks('grunt-open');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+  
     grunt.loadNpmTasks('grunt-contrib-stylus');
-
-    grunt.registerTask('default', ['connect',"open:server",'watch']);
+    grunt.loadNpmTasks('grunt-contrib-watch'); 
+    grunt.registerTask('default', ['watch']);
 };
